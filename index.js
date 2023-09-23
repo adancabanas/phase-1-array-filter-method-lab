8e99add4db0f56488710b10a3623513bc5d505ce
Adan Nicolas Cabanas Garza <adan.cabanas@gmail.com>
2023-09-23 17:07:43 -0400

    defines filter functions

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter((driver) => { if (name.toLowerCase() === driver.toLowerCase()) {
+            return driver;
+        }
+    });
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(driver => {
+        if (driver.toLowerCase().startsWith(name.toLowerCase())) {
+            return driver;
+        }
+    })
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => {
+        if (driver.name.toLowerCase() === name.toLowerCase()) {
+            return driver
+        }
+    })
+}
